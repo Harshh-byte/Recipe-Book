7e6d1800d119e2d97db2bd6ffc381ce80f605172
Harshh-byte <rockstarharsh7jan@gmail.com>
2025-08-03 00:33:42 +0530

    Changes

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-import RecipeContext from "../context/RecipeProvider";
+import RecipeContext from "../context/RecipeContext";
 
 const SingleRecipe = () => {
   const { id } = useParams();
